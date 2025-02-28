797605a24ecdb027ace3bc33b6a7a9d4a6d73cfb
JcobV01 <jakubwadycki@gmail.com>
2025-02-28 13:05:55 +0100

    sekcja informacyjna

--- src/components/FixedBg.jsx
+++ src/components/FixedBg.jsx
@@ -5,7 +5,7 @@ const FixedBg = () => {
   return (
     <div className='h-dvh w-full fixed top-0 z-[-1]'>
         <div className='w-full bg-[#CDA272] opacity-15'></div>
-        <img src={bg} alt="Tło strony" className='absolute w-full h-full object-cover opacity-10'/>
+        <img src={bg} alt="Tło strony" className='absolute w-full h-dvh object-cover opacity-10'/>
     </div>
   )
 }
--- src/components/home/MainDescription.jsx
+++ src/components/home/MainDescription.jsx
@@ -4,16 +4,16 @@ import bg from '../../assets/media/images/mass-hours-bg.webp'
 
 const MainDescription = () => {
   return (
-    <section className='w-[1240px] flex justify-between items-center my-[128px] mx-auto gap-[128px]'>
-        <div className='flex flex-col gap-[32px] flex-1'>
-            <h2 className='text-[30px] font-light max-w-[600px]'>Parafia Wniebowzięcia Najświętszej Maryi Panny w Maciejowicach</h2>
-            <p>Wspólnota o głębokich tradycjach,  łącząca wiernych we wspólnej modlitwie, sakramentach i działaniach duszpasterskich. Jesteśmy miejscem spotkania z Bogiem, otwartym na każdego, kto pragnie pogłębiać swoją wiarę i uczestniczyć w życiu parafialnym.</p>
-            <p>Nasza parafia należy do Diecezji Siedleckiej, obejmuje wiele miejscowości, a jej historia sięga XII wieku. Głównym ośrodkiem życia duchowego jest nasz piękny kościół parafialny w Maciejowicach.</p>
+    <section className='w-[1240px] flex justify-between items-center my-[128px] mx-auto gap-[128px] max-2xl:w-[1000px] max-xl:w-[900px] max-lg:flex-col max-lg:w-[80%] max-lg:gap-[64px]'>
+        <div className='flex flex-col gap-[32px] flex-1 max-xl:gap-[24px]'>
+            <h2 className='text-[30px] font-light max-w-[600px] max-xl:text-[27px] max-lg:text-center max-lg:max-w-full'>Parafia Wniebowzięcia Najświętszej Maryi Panny w Maciejowicach</h2>
+            <p className='max-lg:text-center'>Wspólnota o głębokich tradycjach,  łącząca wiernych we wspólnej modlitwie, sakramentach i działaniach duszpasterskich. Jesteśmy miejscem spotkania z Bogiem, otwartym na każdego, kto pragnie pogłębiać swoją wiarę i uczestniczyć w życiu parafialnym.</p>
+            <p className='max-lg:text-center'>Nasza parafia należy do Diecezji Siedleckiej, obejmuje wiele miejscowości, a jej historia sięga XII wieku. Głównym ośrodkiem życia duchowego jest nasz piękny kościół parafialny w Maciejowicach.</p>
         </div>
-        <div className='w-[350px] h-[400px] relative flex flex-col items-center justify-center gap-[32px]'>
-            <img src={bg} alt="Zdjęcie przedstawiające wnętrze kościoła" width={350} height={400} loading='lazy' className='absolute w-full h-full brightness-25 z-0'/>
+        <div className='w-[350px] h-[400px] relative flex flex-col items-center justify-center gap-[32px] max-sm:w-[100%]'>
+            <img src={bg} alt="Zdjęcie przedstawiające wnętrze kościoła" width={350} height={400} loading='lazy' className='absolute w-full h-full object-cover brightness-25 z-0'/>
 
-            <h2 className='text-white relative z-10 text-[30px] font-light'>Msze święte</h2>
+            <h2 className='text-white relative z-10 text-[30px] font-light max-xl:text-[27px]'>Msze święte</h2>
 
             <div className='relative z-10'>
                 <p className='text-white text-[18px] text-center'>Dni powszednie</p>
