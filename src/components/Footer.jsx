982f1d358b072a9e91f1194b5ffc3731d4beb846
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-03-03 09:09:41 +0100

    Dodanie styli h1,h2,h3 do index.css, prace nad stopką

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,61 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const elements = [
+  {
+    name: "Nasza Parafia",
+    link: "/#parafia"
+  },
+  {
+    name: "Porządek Mszy św. (Informator Parafialny)",
+    link: "/informator"
+  },
+  {
+    name: "Wiadomości z życia Kościoła",
+    link: "/#krp"
+  },
+  {
+    name: "Galeria",
+    link: "/galeria"
+  },
+  {
+    name: "Cmentarz",
+    link: "https://airtilion.com"
+  },
+  {
+    name: "Kontakt",
+    link: "/#kontakt"
+  },
+]
 
 const Footer = () => {
   return (
-    <footer>stopka</footer>
+    <footer className='bg-white'>
+      <div className='w-[80%] mx-auto'>
+
+        <nav className='flex justify-between'>
+          <Link to="/standardy-ochrony-dzieci">Standardy Ochrony Dzieci</Link>
+          <ul className='flex gap-[40px]'>
+            
+            {elements.map((element, index) => (
+              <li key={index} className={`text-[18px] text-center 
+              ${index === 1 ? "max-w-[215px]" : index === 2 ? "max-w-[126px]" : ""}
+              `}>
+                <Link to={element.link}>{element.name}</Link>
+              </li>
+            ))}
+            
+          </ul>
+          <Link to="https://www.facebook.com/groups/336445672064515" target='_blank' rel='noreferrer noopener'>
+            <Icon icon="ic:baseline-facebook" width="25" height="25" alt="Ikona facebook" className='text-[#CDA272]' />
+          </Link>
+        </nav>
+
+        <hr />
+
+      </div>
+    </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <img src={bg} width={1920} height={778} alt="Tło headera" className='absolute object-cover w-full h-full'/>
 
         <img src={Logo} height={200} alt="Logo" className='h-[200px] z-10 max-2xl:h-[160px]'/>
-        <h1 className='text-[35px] w-[560px] z-10 text-white text-center font-light max-2xl:text-[30px] max-md:text-[25px] max-sm:w-[90%]'>Parafia pw. Wniebowzięcia NMP w Maciejowicach</h1>
+        <h1 className='w-[560px] z-10 text-white text-center font-light max-sm:w-[90%]'>Parafia pw. Wniebowzięcia NMP w Maciejowicach</h1>
         <p className='text-white text-[20px] z-10 font-light max-2xl:text-[18px] max-md:text-[15px]'>Diecezja siedlecka</p>
       </section>
     </header>
