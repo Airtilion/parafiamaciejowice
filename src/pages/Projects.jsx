071335d0bac005de8b6a5c452bb6c7d231f0a9d2
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-03-04 18:31:16 +0100

    Dofinansowania edytowalna część z mediami

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import TitleCard from '../components/TitleCard'
 import FixedBg from '../components/FixedBg'
 import NatureEducationTrail from '../components/projects/NatureEducationTrail'
+import RequestForQuote from '../components/projects/RequestForQuote'
 
 const Projects = () => {
   return (
     <>
       <TitleCard title="Dofinansowania" />
-
-
+      <RequestForQuote />
       <NatureEducationTrail />
       <FixedBg />
     </>
