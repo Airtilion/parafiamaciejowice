867524c268f1e25d9d49bbf998f8073f995aa666
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-03-09 18:07:33 +0100

    DOdanie aria-label do wymaganych elementów

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
       <div className='w-[80%] mx-auto flex flex-col justify-center items-center max-2xl:w-[95%]'>
 
         <nav className='w-full flex justify-between text-[16px] text-center py-[32px] max-xl:justify-center max-xl:flex-col max-sm:text-[14px]'>
-          <Link to="/standardy-ochrony-dzieci" className='duration-700 hover:text-[#CDA272]'>Standardy Ochrony Dzieci</Link>
+          <Link to="/standardy-ochrony-dzieci" aria-label="Przejdź do strony Standardy Ochrony Dzieci" className='duration-700 hover:text-[#CDA272]'>Standardy Ochrony Dzieci</Link>
 
           <ul className='flex gap-[40px] max-xl:flex-col max-xl:gap-[16px] max-xl:mt-[16px]'>
 
@@ -50,7 +50,7 @@ const Footer = () => {
 
           </ul>
 
-          <Link to="https://www.facebook.com/groups/336445672064515" target='_blank' rel='noreferrer noopener' className='flex justify-center items-center max-xl:mt-[16px]'>
+          <Link to="https://www.facebook.com/groups/336445672064515" target='_blank' rel='noreferrer noopener' aria-label="Przejdź do facebooka Parafii Maciejowice" className='flex justify-center items-center max-xl:mt-[16px]'>
             <Icon icon="ic:baseline-facebook" width="30" height="30" alt="Ikona facebook" className='text-[#CDA272] duration-700 hover:text-black' />
           </Link>
 
